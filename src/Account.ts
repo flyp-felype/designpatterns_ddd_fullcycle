5c9cfa9bb066a15066774dbd70e0e8f7d1368657
Felype Fernandes <felype@cibus.app.br>
2023-02-21 18:01:07 -0300

    criancao do transfer

---
+++ src/Account.ts
@@ -0,0 +1,41 @@
+import AccountBuilder from "./AccountBuilder";
+import Transaction from "./Transaction";
+
+export default class Account {
+    private bank: string | undefined;
+    private branch: string | undefined;
+    private account: string | undefined;
+    document: string;
+    private transactions: Transaction[]
+
+    constructor(accountBuilder: AccountBuilder) {
+        this.bank = accountBuilder.bank;
+        this.branch = accountBuilder.branch;
+        this.account = accountBuilder.account;
+        this.document = accountBuilder.document
+        this.transactions = []
+    }
+
+    credit(amount: number) {
+        this.transactions.push(new Transaction('credit', amount))
+    }
+
+    debit(amount: number) {
+        this.transactions.push(new Transaction('debit', amount))
+    }
+
+    getBalance() {
+        let balance = 0;
+        for (const transactions of this.transactions) {
+            if (transactions.type === "credit") {
+                balance += transactions.amount
+            }
+            if (transactions.type === "debit") {
+                balance -= transactions.amount
+            }
+
+        }
+
+        return balance;
+    }
+}
